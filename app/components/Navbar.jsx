c64b8cd446fe9ab4947ae61a7100f621fb5a841a
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-01 20:43:10 +0900

    feat: add Navbar component and integrate it into RootLayout chore: clean up Tickets page by removing unnecessary imports

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
+}
