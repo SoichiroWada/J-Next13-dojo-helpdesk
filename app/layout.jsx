cc5cc8a1f54a9eac556ca58fcc1f6b1f875ed993
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-01 20:21:47 +0900

    feat: implement RootLayout and add Tickets page component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,23 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Dojo Helpdesk',
+  description: 'Dojo Helpdesk',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
+
 // import './globals.css'
 // import { Inter } from 'next/font/google'
 
@@ -19,23 +39,3 @@
 //     </html>
 //   )
 // }
-
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Dojo Helpdesk',
-  description: 'Dojo Helpdesk',
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
-  )
-}
