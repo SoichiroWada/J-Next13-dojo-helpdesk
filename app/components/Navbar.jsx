63a752568647d81a24c26c0aeb2c4ae3e4a6689f
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-03 01:00:23 +0900

    feat: enhance navigation in Navbar and TicketList components with links

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Link from "next/link";
-import Image from 'next/image'
-import Logo from './dojo-logo.png'
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <Image
-        src={Logo}
-        alt="Dojo Helpdesk logo"
-        width={70}
-        placeholder="blur"
-        quality={100}
-      />
+      <Link href="/">
+        <Image
+          src={Logo}
+          alt="Dojo Helpdesk logo"
+          width={70}
+          placeholder="blur"
+          quality={100}
+        />
+      </Link>
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
