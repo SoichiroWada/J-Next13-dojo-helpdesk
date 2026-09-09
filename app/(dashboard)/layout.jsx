3b1b61ea9e2dda9b44f30bc0c9aa910ac610c171
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-10 02:14:29 +0900

    fix: clean up imports and commented-out code in DashboardLayout component

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,5 +1,6 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from 'next/headers'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 // components
 import Navbar from "../components/Navbar";
@@ -7,6 +8,13 @@ import Navbar from "../components/Navbar";
 export default async function DashboardLayout({ children }) {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase.auth.getSession();
+
+  // console.log(data);
+  // console.log(data.session);
+
+  // if (!data.session) {
+  //   redirect("/login");
+  // }
 
   return (
     <>
