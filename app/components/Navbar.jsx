cb57adcf4cedaedd7edecbfe621d9e64311cf9e4
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-10 19:24:14 +0900

    feat: implement login functionality with error handling and add LogoutButton component to Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "./dojo-logo.png";
+import LogoutButton from './LogoutButton'
 
 export default function Navbar({user}) {
   return (
@@ -17,8 +18,9 @@ export default function Navbar({user}) {
       </Link>
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
+      <Link href="/tickets" className="mr-auto">Tickets</Link>
       {user && <span>Hello, {user.email}</span>}
+      <LogoutButton></LogoutButton>
     </nav>
   );
 }
