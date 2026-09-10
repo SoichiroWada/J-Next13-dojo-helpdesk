31c719314e4b07ccb40b97aefeb6520820932717
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-10 20:01:14 +0900

    fix: correct link in Navbar component and improve code formatting

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "./dojo-logo.png";
-import LogoutButton from './LogoutButton'
+import LogoutButton from "./LogoutButton";
 
-export default function Navbar({user}) {
+export default function Navbar({ user }) {
   return (
     <nav>
-      <Link href="/scam">
+      <Link href="/">
         <Image
           src={Logo}
           alt="Dojo Helpdesk logo"
@@ -18,7 +18,9 @@ export default function Navbar({user}) {
       </Link>
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
-      <Link href="/tickets" className="mr-auto">Tickets</Link>
+      <Link href="/tickets" className="mr-auto">
+        Tickets
+      </Link>
       {user && <span>Hello, {user.email}</span>}
       <LogoutButton></LogoutButton>
     </nav>
