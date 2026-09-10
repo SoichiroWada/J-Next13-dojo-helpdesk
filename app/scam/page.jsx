3994d105347fe87aefcb17b5f8eb43be4c139a3b
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-10 19:58:35 +0900

    refactor: remove unused scam layout and components

--- app/scam/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Useless() {
-  return (
-    <main>
-      <h2 className="text-center">Are you my children? Yes, we are.</h2>
-    </main>
-  );
-}
--- app/scam/scammer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Scammer() {
-  return (
-    <main>
-      <h3 className="text-center">Oh, hoihowhora! Guru Gueppo Loweg Jueh</h3>
-      <h4 className="text-center">Gueppo Gueppo</h4>
-      <h4 className="text-center">Guero Guero!</h4>
-    </main>
-  );
-}
