3b04144960a6c8f8b896acdd9b74e001d66a7b7b
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-17 02:57:39 +0900

    Upgrade to Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    qualities: [75, 100],
+  },
 
-module.exports = nextConfig
+  allowedDevOrigins: ["192.168.1.68"],
+};
+
+module.exports = nextConfig;
